40186ef95c5fd925f0766e567c80dc3dfa7a8644
Benedikt Klein <benedikt.klein@uni-muenster.de>
2025-10-18 11:28:38 +0200

    Update FOM build.

--- RBInvParam/deployment/run_optimization.py
+++ RBInvParam/deployment/run_optimization.py
@@ -43,7 +43,7 @@ def run_optimization(
 
     ####################################### SETUP FOM #######################################
 
-    FOM, _, _ = build_InstationaryModelIP(setup, logger)
+    FOM = build_InstationaryModelIP(setup, logger)
 
     setup_path = save_path / 'setup.pkl'
     if not setup_path.exists():
