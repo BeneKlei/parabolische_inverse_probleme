2cc656527f526b42dfb5f5d0a7357a21bb935918
Benedikt Klein <benedikt.klein@uni-muenster.de>
2025-11-30 13:20:05 +0100

    Uncomment figsave

--- RBInvParam/optimizer.py
+++ RBInvParam/optimizer.py
@@ -989,24 +989,24 @@ class Optimizer(BasicObject):
                 # projector.pre_compute(center=q)
                 # next_q = projector.project_domain(q, d)
             
-            _u = self.FOM.solve_state(q = self.reductor.reconstruct(q, basis='parameter_basis'))
-            _p = self.FOM.solve_adjoint(q = self.reductor.reconstruct(q, basis='parameter_basis'), u = _u)
+            # _u = self.FOM.solve_state(q = self.reductor.reconstruct(q, basis='parameter_basis'))
+            # _p = self.FOM.solve_adjoint(q = self.reductor.reconstruct(q, basis='parameter_basis'), u = _u)
 
-            u_np = _u.to_numpy()          # shape: (nt, ndofs)
-            nt, ndofs = u_np.shape
+            # u_np = _u.to_numpy()          # shape: (nt, ndofs)
+            # nt, ndofs = u_np.shape
 
-            dof_index = int(1e4)                     # choose any DOF index in [0, ndofs)
-            signal = u_np[:, dof_index]       # shape: (nt,)
+            # dof_index = int(1e4)                     # choose any DOF index in [0, ndofs)
+            # signal = u_np[:, dof_index]       # shape: (nt,)
 
-            signal = signal - np.mean(signal)
+            # signal = signal - np.mean(signal)
 
-            dt = 0.01                         # <-- set this to your actual timestep
-            U_fft = np.fft.rfft(signal)       # complex spectrum, non-negative freqs
-            freqs = np.fft.rfftfreq(nt, d=dt) # frequency axis (Hz)
+            # dt = 0.01                         # <-- set this to your actual timestep
+            # U_fft = np.fft.rfft(signal)       # complex spectrum, non-negative freqs
+            # freqs = np.fft.rfftfreq(nt, d=dt) # frequency axis (Hz)
 
-            ax_3.plot(freqs, np.abs(U_fft))
-            ax_3.grid(True)
-            fig_3.savefig(self.save_path / "fourier_u.pdf")
+            # ax_3.plot(freqs, np.abs(U_fft))
+            # ax_3.grid(True)
+            # fig_3.savefig(self.save_path / "fourier_u.pdf")
 
 
             # u_r = self.reductor.reconstruct(u, basis='state_basis')
@@ -1048,24 +1048,24 @@ class Optimizer(BasicObject):
      
 
 
-            basis = 'state_basis'
-            _basis = self.reductor.bases[basis]
+            # basis = 'state_basis'
+            # _basis = self.reductor.bases[basis]
             
-            coeff_u = np.sum((_u.inner(_basis, self.reductor.products[basis]))**2, axis=0)
-            err_i_u = np.sum(self.reductor.products[basis].pairwise_apply2(_u,_u)) - np.cumsum(coeff_u)
+            # coeff_u = np.sum((_u.inner(_basis, self.reductor.products[basis]))**2, axis=0)
+            # err_i_u = np.sum(self.reductor.products[basis].pairwise_apply2(_u,_u)) - np.cumsum(coeff_u)
             
-            coeff_p = np.sum((_p.inner(_basis, self.reductor.products[basis]))**2, axis=0)
-            err_i_p = np.sum(self.reductor.products[basis].pairwise_apply2(_p,_p)) - np.cumsum(coeff_p)
+            # coeff_p = np.sum((_p.inner(_basis, self.reductor.products[basis]))**2, axis=0)
+            # err_i_p = np.sum(self.reductor.products[basis].pairwise_apply2(_p,_p)) - np.cumsum(coeff_p)
 
-            self.I += 1
-            #color = cmap(self.I)
-            color = cmap(i)
-            ax_1.semilogy(err_i_u, color=color)
-            ax_1.semilogy(err_i_p, color=color, linestyle="--")
-            ax_1.set_ylim([1e-8, 1e3])
-            ax_1.grid(True)
+            # self.I += 1
+            # #color = cmap(self.I)
+            # color = cmap(i)
+            # ax_1.semilogy(err_i_u, color=color)
+            # ax_1.semilogy(err_i_p, color=color, linestyle="--")
+            # ax_1.set_ylim([1e-8, 1e3])
+            # ax_1.grid(True)
 
-            fig_1.savefig(self.save_path / "inner_plot.pdf")
+            # fig_1.savefig(self.save_path / "inner_plot.pdf")
 
 
             # _u_r = model.solve_state(q)
@@ -1842,25 +1842,25 @@ class QrVrROMOptimizer(Optimizer):
 
         ############################################################
 
-        basis = 'state_basis'
-        _basis = self.reductor.bases[basis]
+        # basis = 'state_basis'
+        # _basis = self.reductor.bases[basis]
 
-        coeff_u = np.sum((u.inner(_basis, self.reductor.products[basis]))**2, axis=0)
-        err_i_u = np.sum(self.reductor.products[basis].pairwise_apply2(u,u)) - np.cumsum(coeff_u)
+        # coeff_u = np.sum((u.inner(_basis, self.reductor.products[basis]))**2, axis=0)
+        # err_i_u = np.sum(self.reductor.products[basis].pairwise_apply2(u,u)) - np.cumsum(coeff_u)
         
-        coeff_p = np.sum((p.inner(_basis, self.reductor.products[basis]))**2, axis=0)
-        err_i_p = np.sum(self.reductor.products[basis].pairwise_apply2(p,p)) - np.cumsum(coeff_p)
+        # coeff_p = np.sum((p.inner(_basis, self.reductor.products[basis]))**2, axis=0)
+        # err_i_p = np.sum(self.reductor.products[basis].pairwise_apply2(p,p)) - np.cumsum(coeff_p)
 
-        # err_i_u = err_i_u[err_i_u > 0]
-        # err_i_p = err_i_p[err_i_p > 0]
+        # # err_i_u = err_i_u[err_i_u > 0]
+        # # err_i_p = err_i_p[err_i_p > 0]
 
-        color = cmap(i)
-        ax_2.semilogy(err_i_u, color=color)
-        ax_2.semilogy(err_i_p, color=color, linestyle="--")
-        ax_2.set_ylim([1e-18, 1e3])
-        ax_2.grid(True)
+        # color = cmap(i)
+        # ax_2.semilogy(err_i_u, color=color)
+        # ax_2.semilogy(err_i_p, color=color, linestyle="--")
+        # ax_2.set_ylim([1e-18, 1e3])
+        # ax_2.grid(True)
         
-        fig_2.savefig(self.save_path / "coeffs_after_enrich.pdf")
+        # fig_2.savefig(self.save_path / "coeffs_after_enrich.pdf")
 
         ############################################################
 
@@ -2588,28 +2588,28 @@ class QrVrROMOptimizer(Optimizer):
 
                     ############################################################
 
-                    basis = 'state_basis'
-                    _basis = self.reductor.bases[basis]
+                    # basis = 'state_basis'
+                    # _basis = self.reductor.bases[basis]
 
-                    coeff_u = np.sum((u.inner(_basis, self.reductor.products[basis]))**2, axis=0)
-                    err_i_u = np.sum(self.reductor.products[basis].pairwise_apply2(u,u)) - np.cumsum(coeff_u)
+                    # coeff_u = np.sum((u.inner(_basis, self.reductor.products[basis]))**2, axis=0)
+                    # err_i_u = np.sum(self.reductor.products[basis].pairwise_apply2(u,u)) - np.cumsum(coeff_u)
                     
-                    coeff_p = np.sum((p.inner(_basis, self.reductor.products[basis]))**2, axis=0)
-                    err_i_p = np.sum(self.reductor.products[basis].pairwise_apply2(p,p)) - np.cumsum(coeff_p)
+                    # coeff_p = np.sum((p.inner(_basis, self.reductor.products[basis]))**2, axis=0)
+                    # err_i_p = np.sum(self.reductor.products[basis].pairwise_apply2(p,p)) - np.cumsum(coeff_p)
 
-                    # err_i_u = err_i_u[err_i_u > 0]
-                    # err_i_p = err_i_p[err_i_p > 0]
+                    # # err_i_u = err_i_u[err_i_u > 0]
+                    # # err_i_p = err_i_p[err_i_p > 0]
                     
-                    print(err_i_u[-1])
-                    print(err_i_p[-1])
+                    # print(err_i_u[-1])
+                    # print(err_i_p[-1])
 
-                    color = cmap(i+1)
-                    ax_2.semilogy(err_i_u, color=color)
-                    ax_2.semilogy(err_i_p, color=color, linestyle="--")
-                    ax_2.set_ylim([1e-18, 1e3])
-                    ax_2.grid(True)
+                    # color = cmap(i+1)
+                    # ax_2.semilogy(err_i_u, color=color)
+                    # ax_2.semilogy(err_i_p, color=color, linestyle="--")
+                    # ax_2.set_ylim([1e-18, 1e3])
+                    # ax_2.grid(True)
                     
-                    fig_2.savefig(self.save_path / "coeffs_after_enrich.pdf")
+                    # fig_2.savefig(self.save_path / "coeffs_after_enrich.pdf")
 
 
 
