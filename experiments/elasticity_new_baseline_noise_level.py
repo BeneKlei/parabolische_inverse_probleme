73482cf87bc79a179c0f681632b597b717bc47d0
Benedikt Klein <benedikt.klein@uni-muenster.de>
2025-12-17 09:20:05 +0100

    Correct experiment config.

--- experiments/elasticity_new_baseline_noise_level.py
+++ experiments/elasticity_new_baseline_noise_level.py
@@ -276,7 +276,7 @@ for noise_level in noise_levels:
 
     # EXPERIMENTS['FOM_sensors'] = (setup_sensors, FOM_optimizer_parameter_sensors)
     # EXPERIMENTS['FOM_identity'] = (setup_identity, FOM_optimizer_parameter_identity)
-    EXPERIMENTS['FOM_grid'] = (setup_grid, FOM_optimizer_parameter_grid)
+    _EXPERIMENTS['FOM_grid'] = (setup_grid, FOM_optimizer_parameter_grid)
 
     #----------------------------------------------------------------------------------------
 
@@ -295,7 +295,7 @@ for noise_level in noise_levels:
 
     # EXPERIMENTS['TR_sensors_time_step'] = (setup_sensors, TR_optimizer_parameter_sensors)
     # EXPERIMENTS['TR_identity_time_step'] = (setup_identity, TR_optimizer_parameter_identity)
-    EXPERIMENTS['TR_grid_time_step'] = (setup_grid, TR_optimizer_parameter_grid)
+    _EXPERIMENTS['TR_grid_time_step'] = (setup_grid, TR_optimizer_parameter_grid)
 
 
     #----------------------------------------------------------------------------------------
@@ -311,9 +311,9 @@ for noise_level in noise_levels:
     #TR_optimizer_parameter_identity['lin_solver_parms']['lin_solver_tol'] = identity_lin_solver_tol
     TR_optimizer_parameter_grid['lin_solver_parms']['lin_solver_tol'] = grid_lin_solver_tol
 
-    EXPERIMENTS['TR_sensors_time_step_full'] = (setup_sensors, TR_optimizer_parameter_sensors)
-    EXPERIMENTS['TR_identity_time_step_full'] = (setup_identity, TR_optimizer_parameter_identity)
-    EXPERIMENTS['TR_grid_time_step_full'] = (setup_grid, TR_optimizer_parameter_grid)
+    #_EXPERIMENTS['TR_sensors_time_step_full'] = (setup_sensors, TR_optimizer_parameter_sensors)
+    #_EXPERIMENTS['TR_identity_time_step_full'] = (setup_identity, TR_optimizer_parameter_identity)
+    _EXPERIMENTS['TR_grid_time_step_full'] = (setup_grid, TR_optimizer_parameter_grid)
 
 
     #----------------------------------------------------------------------------------------
@@ -328,9 +328,9 @@ for noise_level in noise_levels:
     TR_optimizer_parameter_identity['lin_solver_parms']['lin_solver_tol'] = identity_lin_solver_tol
     TR_optimizer_parameter_grid['lin_solver_parms']['lin_solver_tol'] = grid_lin_solver_tol
 
-    # EXPERIMENTS['TR_sensors'] = (setup_sensors, TR_optimizer_parameter_sensors)
-    # EXPERIMENTS['TR_identity'] = (setup_identity, TR_optimizer_parameter_identity)
-    EXPERIMENTS['TR_grid'] = (setup_grid, TR_optimizer_parameter_grid)
+    # _EXPERIMENTS['TR_sensors'] = (setup_sensors, TR_optimizer_parameter_sensors)
+    # _EXPERIMENTS['TR_identity'] = (setup_identity, TR_optimizer_parameter_identity)
+    _EXPERIMENTS['TR_grid'] = (setup_grid, TR_optimizer_parameter_grid)
 
     prefix = f'new_baseline_noise_level_{noise_level}'
     _EXPERIMENTS = {f"{prefix}_{k}": v for k, v in _EXPERIMENTS.items()}
